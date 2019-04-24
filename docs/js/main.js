4a61da47b1312dc51c25ea63f8600ae955749aec
Aliaksei Sachavichyk <roim@ya.ru>
2019-04-24 21:08:10 +0300

    Adaptive layout + Popup Menu

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,2 +1,22 @@
-"use strict";
+var button = document.getElementById('button');
+var menu = document.getElementById('menu');
+var lines = document.getElementsByClassName('mb-line');
 
+button.addEventListener('click', function(){
+	menu.style.display = menu.style.display === 'none' ? 'flex' : 'none';
+});
+
+window.addEventListener('resize', function(){
+	document.documentElement.clientWidth > 992 ? menu.style.display = 'flex' : menu.style.display = 'none';
+});
+
+document.addEventListener('click', function(e){
+	if (e.target !== menu &&
+		e.target !== button &&
+		e.target !== button.childNotes &&
+		menu.style.display === 'flex' &&
+		document.documentElement.clientWidth < 991
+	) {
+		menu.style.display = 'none';
+	}
+});
